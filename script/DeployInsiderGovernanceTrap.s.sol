e3e81dff1c12d5c386e506f696a7e37497b1ab0a
emark-cloud <eakinleye97dami@gmail.com>
2026-03-08 07:44:24 +0100

    Update DeployInsiderGovernanceTrap.s.sol

--- script/DeployInsiderGovernanceTrap.s.sol
+++ script/DeployInsiderGovernanceTrap.s.sol
@@ -4,25 +4,43 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import {InsiderGovernanceManipulationTrap} from "../src/InsiderGovernanceManipulationTrap.sol";
 import {InsiderGovernanceResponder} from "../src/InsiderGovernanceResponder.sol";
+import {InsiderGovernanceFeeder} from "../src/InsiderGovernanceFeeder.sol";
 
 contract DeployInsiderGovernanceTrapScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // Replace with deployed feeder OR deploy feeder here
-        address feederAddress = 0x0000000000000000000000000000000000000001;
+        /* ---------------------------------------------------------- */
+        /* Deploy Feeder                                              */
+        /* ---------------------------------------------------------- */
 
-        // 1. Deploy trap (FIXED)
+        InsiderGovernanceFeeder feeder =
+            new InsiderGovernanceFeeder(msg.sender);
+
+        /* ---------------------------------------------------------- */
+        /* Deploy Trap (no constructor args)                          */
+        /* ---------------------------------------------------------- */
+
         InsiderGovernanceManipulationTrap trap =
-            new InsiderGovernanceManipulationTrap(feederAddress);
+            new InsiderGovernanceManipulationTrap();
 
-        // 2. Deploy responder (no args)
+        /* ---------------------------------------------------------- */
+        /* Configure Trap                                             */
+        /* ---------------------------------------------------------- */
+
+        trap.setFeeder(address(feeder));
+
+        /* ---------------------------------------------------------- */
+        /* Deploy Responder                                           */
+        /* ---------------------------------------------------------- */
+
         InsiderGovernanceResponder responder =
             new InsiderGovernanceResponder();
 
         vm.stopBroadcast();
 
+        console2.log("Feeder deployed at:", address(feeder));
         console2.log("Trap deployed at:", address(trap));
         console2.log("Responder deployed at:", address(responder));
     }
